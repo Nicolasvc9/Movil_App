902e7f9e5c45fef4d7be2f79fe85a4f7880daff5
Nicolasvc9 <nicolasvargas0624@gmail.com>
2023-05-06 14:12:34 -0500

    agg PhonesPage

---
+++ src/pages/phones.vue
@@ -0,0 +1,42 @@
+<template>
+    <f7-page>
+        <f7-navbar title="Celulares" back-link="Back"></f7-navbar>
+        <f7-block-title>MOBILES</f7-block-title>
+        <f7-list media-list dividers-ios strong-ios outline-ios>
+            <f7-list-item link="#" title="iPhone 14 Pro" after="$1.000" subtitle="Apple"
+                text="Una manera mágica de interactuar con tu iPhone. Una funcionalidad de seguridad diseñada para salvar vidas. Una innovadora cámara gran angular de 48 MP. Y una pantalla hasta dos veces más brillante bajo el sol.◊Consultar los avisos legales Todo gracias a la potencia del ultrarrápido chip A16 Bionic.">
+                <template #media>
+                    <img style="border-radius: 8px"
+                        src="https://lh3.googleusercontent.com/PN7U9AA1v_BYPG5kLhZOMjINsFXK0SXqKXUXxlutI_81ovOI61jxUM-jDxk_jLnvhjUXbw2NmfCtvHHqceDEgWFQyBgTFfpxSy4jtytX"
+                        width="80" />
+                </template>
+            </f7-list-item>
+            <f7-list-item link="#" title="Galaxy S23 Ultra 5G" after="$1.000" subtitle="samsung"
+                text="Ahora es más fácil que nunca configurar tu smartphone solo para ti. One UI maximiza la personalización, lo que te permite elegir casi todos los detalles, desde pantallas de bloqueo y temas hasta widgets y notificaciones.">
+                <template #media>
+                    <img style="border-radius: 8px"
+                        src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSUWNXO2IdzQHe2Y0CXmLu7nn3JLoMjIe74-TPmaAvIlDLBgZS0"
+                        width="80" />
+                </template>
+            </f7-list-item>
+            <f7-list-item link="#" title="Xiaomi 12T Pro" after="$1.000" subtitle="Xiaomi"
+                text="La ampliación de memoria solo está disponible cuando hay suficiente espacio de almacenamiento en el dispositivo.
+La garantía no cubre mal uso por golpes y/o humedad, mala actualización del sistema y/o cualquier otro caso de mal uso del dispositivo.
+El almacenamiento y la RAM disponibles son inferiores a la memoria total debido al almacenamiento del sistema operativo y el software preinstalado en el dispositivo.">
+                <template #media>
+                    <img style="border-radius: 8px"
+                        src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRoPcxWYidXeJ2jIxSM_kXb2-U-uTm5dNgipmJAnvS08fbGvfXP"
+                        width="80" />
+                </template>
+            </f7-list-item>
+            <f7-list-item link="#" title="Motorola Edge 30 Ultra" after="$1.000" subtitle="Motorola"
+                text="El nuevo motorola edge 30 ultra viene con la plataforma móvil Snapdragon líder en la industria y más avanzada hasta el momento. La plataforma móvil Snapdragon 8+ Gen 1 lidera el camino hacia una nueva era de tecnología móvil premium equipada con 5G">
+                <template #media>
+                    <img style="border-radius: 8px"
+                        src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSpl86IvxZE39FA-m87cmOpgSJ1RsD_0NlLQDeyNxZ3d8MW1WtH"
+                        width="80" />
+                </template>
+            </f7-list-item>
+        </f7-list>
+    </f7-page>
+</template>
