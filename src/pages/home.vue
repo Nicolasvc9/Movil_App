9b06feddb6c9d2f0f41e68dd36aa76a30204164e
Nicolasvc9 <nicolasvargas0624@gmail.com>
2023-05-06 14:48:42 -0500

    add new pages

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -63,6 +63,79 @@ El almacenamiento y la RAM disponibles son inferiores a la memoria total debido 
           </template>
         </f7-list-item>
       </f7-list>
+      <f7-block-title>ACCESORIOS</f7-block-title>
+      <f7-list media-list dividers-ios strong-ios outline-ios>
+        <f7-list-item link="#" title="Cargador de pared Apple USB-C 20W" after="$1.000" subtitle="Apple"
+          text="El adaptador de corriente USB-C de 20 W de Apple es muy rápido y eficiente, así que va de perlas para cargar tus dispositivos en cualquier lugar. 
+                Aunque es compatible con cualquier dispositivo con USB-C, Apple recomienda usarlo con el iPad Pro y el iPad Air para lograr un rendimiento óptimo. 
+                Puedes usarlo con un iPhone 8 o posterior y aprovechar la carga rápida. El cable de carga se vende por separado.">
+          <template #media>
+            <img style="border-radius: 8px"
+              src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcT2lB5SvRFxBIgAS3ZuUM7x9Bz4a0eip2ekSCDLb3uWhiwkiGXT"
+              width="80" />
+          </template>
+        </f7-list-item>
+        <f7-list-item link="#" title="Cable Apple de USB-C a Conector Lightning (1m) " after="$1.000" subtitle="Apple"
+          text="Conecte su iPhone, iPad o iPod con conector Lightning a su Mac compatible con USB-C o Thunderbolt 3 (USB-C) para sincronizar y cargar, o a su iPad compatible con USB-C para cargar.">
+          <template #media>
+            <img style="border-radius: 8px"
+              src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSFLEp64qFjFRl6gk2lvHZAj_DqtZ3pSmVoWqbKP5w75fpHmfnh"
+              width="80" />
+          </template>
+        </f7-list-item>
+        <f7-list-item link="#" title="Base de Carga Inálambrica Belkin 3 en 1 -" after="$1.000" subtitle="Belkin"
+          text="Cargue sus dispositivos Apple más rápido con un soporte de carga potente y de hermoso diseño que cuenta con el último módulo magnético de carga rápida para Apple Watch Series 8 y carga MagSafe oficial de hasta 15 W para su iPhoneContenido de la Caja: BOOST↑CHARGE PRO MagSafe 3-in-1 Wireless Charging Stand with MagSafe
+AC AdapterIPHONE: iPhone 14 Pro Max, iPhone 14 Pro, iPhone 14 Plus, iPhone 14, iPhone 13 Pro Max, iPhone 13 Pro, iPhone 13, iPhone 13 mini, iPhone 12 Pro Max, iPhone 12 Pro, iPhone 12, iPhone 12 miniWATCH: Watch SE (40mm), Watch SE (44mm), Watch Series 7 (41mm), Watch Series 7 (45mm), Watch Series 8 (41mm), Watch Series 8 (45mm), Watch Ultra (49mm)AIRPODS">
+          <template #media>
+            <img style="border-radius: 8px"
+              src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRcNts3uDoSeV6iol_YjodenyiEw4Z869lf_DaKbfaEJ2lpyVws"
+              width="80" />
+          </template>
+        </f7-list-item>
+        <f7-list-item link="#" title="Cargador de pared 37W USB-C 25W + USB-A 12W" after="$1.000" subtitle="Belkin"
+          text="Este cargador cuenta con la certificación USB-C PD 3.0 y ofrece velocidades de carga rápidas de hasta 0-50 % en 30 minutos. Está diseñado con un tamaño compacto que se adapta a cualquier entorno. Perfecto para hogares u oficinas">
+          <template #media>
+            <img style="border-radius: 8px"
+              src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQUn8SPp2VQKOngMZSTFEwU5rXwosjOOA4EX4lEL3GZdnfHDCJA"
+              width="80" />
+          </template>
+        </f7-list-item>
+      </f7-list>
+      <f7-block-title>AUDIO</f7-block-title>
+      <f7-list media-list dividers-ios strong-ios outline-ios>
+        <f7-list-item link="#" title="AirPods (3ra generación) con carga MagSafe" after="$1.000" subtitle="Apple"
+          text="Los AirPods no pesan nada y ofrecen un ajuste anatómico. Se colocan en el ángulo perfecto para darte un mayor confort y llevar el sonido directamente a tus oídos. Además, son un 33 % más cortos que los AirPods (2.ª generación) e incluyen un sensor de presión que te permite controlar la música y las llamadas fácilmente.">
+          <template #media>
+            <img style="border-radius: 8px"
+              src="https://tiendasishop.com/media/catalog/product/m/m/mme73am_a.jpg?optimize=high&bg-color=255,255,255&fit=bounds&height=700&width=700&canvas=700:700&dpr=2%202x"
+              width="80" />
+          </template>
+        </f7-list-item>
+        <f7-list-item link="#" title="AirPods Pro (2da Generación)" after="$1.000" subtitle="Apple"
+          text="Conecte su iPhone, iPad o iPod con conector Lightning a su Mac compatible con USB-C o Thunderbolt 3 (USB-C) para sincronizar y cargar, o a su iPad compatible con USB-C para cargar.">
+          <template #media>
+            <img style="border-radius: 8px"
+              src="https://tiendasishop.com/media/catalog/product/a/i/airpods_pro_2nd_gen_pdp_image_position-2__usen.jpg?optimize=high&bg-color=255,255,255&fit=bounds&height=700&width=700&canvas=700:700&dpr=2%202x"
+              width="80" />
+          </template>
+        </f7-list-item>
+        <f7-list-item link="#" title="Audífonos Jabra In Ear inalámbroc Elite 3 Bluetooth" after="$1.000" subtitle="Jabra"
+          text="Jabra Elite 3 están hechos para dar nueva vida a tu música. Diseñados para ofrecer graves intensos y completos, un potente sonido personalizable y 4 micrófonos para llamadas nítidas donde quiera que vayas. Te brindan hasta 21 horas de autonomía con el estuche de carga.Con 7 horas de autonomía en los audífonos y un total de 28 horas con el estuche. Una carga rápida de 10 minutos te brinda 1 hora de reproducciónContenido de la Caja">
+          <template #media>
+            <img style="border-radius: 8px"
+              src="https://tiendasishop.com/media/catalog/product/1/0/100-91410001-60.jpg?optimize=high&bg-color=255,255,255&fit=bounds&height=700&width=700&canvas=700:700&dpr=2%202x"
+              width="80" />
+          </template>
+        </f7-list-item>
+        <f7-list-item link="#" title="Audífonos Beats Solo3 Wireless Headphones" after="$1.000" subtitle="Beats"
+          text="Los auriculares Beats Solo3 Wireless ofrecen una experiencia de reproducción prémium con gran precisión acústica para disfrutar de un sonido claro, nítido y equilibrado.">
+          <template #media>
+            <img style="border-radius: 8px"
+              src="https://tiendasishop.com/media/catalog/product/m/x/mx432_av4.jpeg?optimize=high&bg-color=255,255,255&fit=bounds&height=700&width=700&canvas=700:700&dpr=2%202x"
+              width="80" />
+          </template>
+        </f7-list-item>
+      </f7-list>
     </f7-page>
   </f7-page>
 </template>
