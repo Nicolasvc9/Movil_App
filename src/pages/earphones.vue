ef8df9d7c08c6fd9b0c3d6274311f501afa09da2
Nicolasvc9 <nicolasvargas0624@gmail.com>
2023-05-06 14:43:22 -0500

    earphonespage

---
+++ src/pages/earphones.vue
@@ -0,0 +1,40 @@
+<template>
+    <f7-page>
+        <f7-navbar title="Audifonos" back-link="Back"></f7-navbar>
+        <f7-block-title>AUDIO</f7-block-title>
+        <f7-list media-list dividers-ios strong-ios outline-ios>
+            <f7-list-item link="#" title="AirPods (3ra generación) con carga MagSafe" after="$1.000" subtitle="Apple"
+                text="Los AirPods no pesan nada y ofrecen un ajuste anatómico. Se colocan en el ángulo perfecto para darte un mayor confort y llevar el sonido directamente a tus oídos. Además, son un 33 % más cortos que los AirPods (2.ª generación) e incluyen un sensor de presión que te permite controlar la música y las llamadas fácilmente.">
+                <template #media>
+                    <img style="border-radius: 8px"
+                        src="https://tiendasishop.com/media/catalog/product/m/m/mme73am_a.jpg?optimize=high&bg-color=255,255,255&fit=bounds&height=700&width=700&canvas=700:700&dpr=2%202x"
+                        width="80" />
+                </template>
+            </f7-list-item>
+            <f7-list-item link="#" title="AirPods Pro (2da Generación)" after="$1.000" subtitle="Apple"
+                text="Conecte su iPhone, iPad o iPod con conector Lightning a su Mac compatible con USB-C o Thunderbolt 3 (USB-C) para sincronizar y cargar, o a su iPad compatible con USB-C para cargar.">
+                <template #media>
+                    <img style="border-radius: 8px"
+                        src="https://tiendasishop.com/media/catalog/product/a/i/airpods_pro_2nd_gen_pdp_image_position-2__usen.jpg?optimize=high&bg-color=255,255,255&fit=bounds&height=700&width=700&canvas=700:700&dpr=2%202x"
+                        width="80" />
+                </template>
+            </f7-list-item>
+            <f7-list-item link="#" title="Audífonos Jabra In Ear inalámbroc Elite 3 Bluetooth" after="$1.000" subtitle="Jabra"
+                text="Jabra Elite 3 están hechos para dar nueva vida a tu música. Diseñados para ofrecer graves intensos y completos, un potente sonido personalizable y 4 micrófonos para llamadas nítidas donde quiera que vayas. Te brindan hasta 21 horas de autonomía con el estuche de carga.Con 7 horas de autonomía en los audífonos y un total de 28 horas con el estuche. Una carga rápida de 10 minutos te brinda 1 hora de reproducciónContenido de la Caja">
+                <template #media>
+                    <img style="border-radius: 8px"
+                        src="https://tiendasishop.com/media/catalog/product/1/0/100-91410001-60.jpg?optimize=high&bg-color=255,255,255&fit=bounds&height=700&width=700&canvas=700:700&dpr=2%202x"
+                        width="80" />
+                </template>
+            </f7-list-item>
+            <f7-list-item link="#" title="Audífonos Beats Solo3 Wireless Headphones" after="$1.000" subtitle="Beats"
+                text="Los auriculares Beats Solo3 Wireless ofrecen una experiencia de reproducción prémium con gran precisión acústica para disfrutar de un sonido claro, nítido y equilibrado.">
+                <template #media>
+                    <img style="border-radius: 8px"
+                        src="https://tiendasishop.com/media/catalog/product/m/x/mx432_av4.jpeg?optimize=high&bg-color=255,255,255&fit=bounds&height=700&width=700&canvas=700:700&dpr=2%202x"
+                        width="80" />
+                </template>
+            </f7-list-item>
+        </f7-list>
+    </f7-page>
+</template>
